08972fb085a5a39ea9a5c1554574d0dac992340b
DINESH THIRU <dineshthiru36@gmail.com>
2023-10-02 17:03:14 +0530

    Updates

--- frontend/src/Components/01-10-23/SingleProductPage.jsx
+++ frontend/src/Components/01-10-23/SingleProductPage.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import "./SingleProductPage.css"
 
 const SingleProductPage = () => {
    const [productData, setProductData] = useState({})
@@ -9,7 +10,7 @@ const SingleProductPage = () => {
       async function getSingleProductData() {
          try {
             const { data } = await axios.get(`https://fakestoreapi.com/products/${id}`)
-            if(data){
+            if (data) {
                setProductData(data)
             }
          } catch (error) {
@@ -24,22 +25,36 @@ const SingleProductPage = () => {
 
    return (
       <div>
-         {productData?.id?
-         <div className="single-pro-pg">
-            <div className="single-pro-img">
-               <img src={productData.image} alt="" />
+         {productData?.id ?
+            <div className="single-pro-pg">
+               <div className="single-pro-img">
+                  <img src={productData.image} alt="" />
+               </div>
+               <div className="single-pro-info">
+                  <div className="single-pro-nav">
+                     <p className="single-pro-cat">Category:{productData.category}</p>
+                     <div className="single-pro-home-logo">Home</div>
+                  </div>
+
+                  <h1>{productData.title}</h1>
+
+                  <div className="single-pro-rating">
+                     <p>Rating:{productData.rating.rate}</p>
+                     <p>no of ratings:{productData.rating.count}</p>
+                  </div>
+
+                  <p>Description:{productData.description}</p>
+                  
+                  <p>Price:{productData.price}$</p>
+
+                  <div className="single-pro-buttons">
+                     <button>ADD TO BAG</button>
+                     <button>SAVE TO WISHLIST</button>
+                  </div>
+               </div>
             </div>
-            <div className="single-pro-info">
-               <h1>{productData.title}</h1>
-               <p>Category:{productData.category}</p>
-               <p>Description:{productData.description}</p>
-               <p>Price:{productData.price}</p>
-               <p>Rating:{productData.rating.rate}</p>
-               <p>no of ratings:{productData.rating.count}</p>
-            </div>
-         </div>
-         :
-         <div>Loading...</div>
+            :
+            <div>Loading...</div>
          }
       </div>
    )
