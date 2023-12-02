78d6cd0de23862b2bbdc081dfab40c73d69d367f
DINESH THIRU <dineshthiru36@gmail.com>
2023-12-02 13:24:03 +0530

    My-store

--- frontend/src/Components/Cart/Cart.jsx
+++ frontend/src/Components/Cart/Cart.jsx
@@ -21,6 +21,18 @@ const Cart = () => {
     }
   }
 
+  async function deleteCartProduct(proId) {
+    try {
+      const response = await api.post("user/delete-cart", { proId, userId: state?.user?.id })
+      if (response.data.success) {
+        toast.success(response.data.message)
+        setCartProducts(response.data.products)
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
 
   useEffect(() => {
     try {
@@ -42,8 +54,26 @@ const Cart = () => {
   return (
     <>
       <Navbar />
-      <div>
-        Cart
+      <h1>Cart</h1>
+      <div className="ypro-layout">
+        {cartProducts.length && cartProducts.map((pro) => (
+          <div className="ypro-content" key={pro._id}>
+            <div className="ypro-img-box">
+              <img src={pro.image} alt="product-img" className="ypro-img" />
+            </div>
+            <div className="ypro-text-layout">
+              <h1 className="ypro-name">{pro.name}</h1>
+              <div className="ypro-text">
+                <p className="ypro-price">Price: {pro.price}$</p>
+                <div className="add-cart-layout">
+                  <button className="add-cart1" onClick={() => router(`/single-product-page/${pro._id}`)}>View</button>
+                  <button className="add-cart2" onClick={() => deleteCartProduct(pro._id)}>Delete</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))
+        }
       </div>
     </>
   )
