32f3c7197b67bccd438a3f52840ddff7ae0576c3
DINESH THIRU <dineshthiru36@gmail.com>
2023-12-08 01:58:24 +0530

    Updated

--- frontend/src/Helpers/Axios.Config.js
+++ frontend/src/Helpers/Axios.Config.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const api = axios.create({baseURL:"http://localhost:8000/api/v1"})
+const api = axios.create({baseURL:"https://backend-eebv.onrender.com/api/v1"})
 
 export default api;
